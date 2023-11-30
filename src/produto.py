868bc1a4d87dc8aac737064cc11af1e463cd9710
alanguilhermeM <alanguilherme13@outlook.com>
2023-11-30 12:55:43 -0300

    FInalizado requisito 1

--- src/produto.py
+++ src/produto.py
@@ -2,19 +2,35 @@ class Produto:
     def __init__(
         self, nome: str, codigo: str, preco: float, quantidade: int
     ) -> None:
-        pass
+        self.__nome = nome
+        self.__codigo = codigo
+        self.__preco = preco
+        self.__quantidade = quantidade
 
     def get_preco(self) -> float:
-        pass
+        return self.__preco
 
     def get_quantidade(self) -> int:
-        pass
+        return self.__quantidade
 
     def atualizar_preco_do_produto(self, novo_preco: float) -> None:
-        pass
+        if novo_preco < 0:
+            raise ValueError("Preço não pode ser negativo")
+
+        self.__preco = novo_preco
 
     def adicionar_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        if self.__quantidade < 0:
+            raise ValueError("Quantidade não pode ser negativo")
+
+        self.__quantidade += quantidade
 
     def remover_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        if self.__quantidade < quantidade:
+            raise ValueError(f'Estoque insuficiente: {self.__quantidade}')
+
+        self.__quantidade -= quantidade
+
+
+instance = Produto('Notebook', 'A3dh4', 2, 1)
+print(instance.atualizar_preco_do_produto(2))
