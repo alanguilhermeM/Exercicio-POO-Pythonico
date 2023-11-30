ff567e506967f0e234ce6e53b1908d5010515799
alanguilhermeM <alanguilherme13@outlook.com>
2023-11-30 14:17:29 -0300

    FInalizado requisito 3

--- tests/livro/test_livro.py
+++ tests/livro/test_livro.py
@@ -1,5 +1,8 @@
-# from src.livro.livro import Livro
+from src.livro.livro import Livro
 
 
 def test_cria_livro():
-    pass  # Seu teste deve ser escrito aqui
+    li = Livro('Harry Hotter', 'J.K.Roulins', 320)
+    assert li.titulo == "Harry Hotter"
+    assert li.autor == "J.K.Roulins"
+    assert li.paginas == 320
